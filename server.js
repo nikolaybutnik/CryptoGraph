252c350d914dfae329e34ddbc3a91ec3dd477543
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-03-24 20:49:59 -0400

    feat: connect to Binance API

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3001
 const app = express()
+const ccxt = require('ccxt')
+const axios = require('axios')
+
+require('dotenv').config()
 
 // Define middleware here
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
@@ -12,6 +16,14 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Define API routes here
+const tick = async (config, binanceClient) => {}
+
+const run = () => {
+  const binanceClient = new ccxt.binance({
+    apiKey: process.env.REACT_APP_API_KEY,
+    secret: process.env.REACT_APP_API_SECRET,
+  })
+}
 
 // Send every other request to the React app
 // Define any API routes before this runs
