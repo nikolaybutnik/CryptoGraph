feef096929566bf2381fba7de9f0cbbb4d2754ac
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-03-29 21:15:22 -0400

    feat: change to line chart and limit number of ticks to 12

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Bar } from 'react-chartjs-2'
+import { Line } from 'react-chartjs-2'
 
 const Chart = ({ labels, datasets }) => {
   const data = {
@@ -7,25 +7,49 @@ const Chart = ({ labels, datasets }) => {
     datasets: [
       {
         label: 'ETH/USDT',
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
-        borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
         data: datasets,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
       },
     ],
   }
+  console.log(data)
 
   return (
     <div>
       <h2>Bar Example (custom size)</h2>
-      <Bar
+      <Line
         data={data}
-        width={100}
-        height={50}
+        // width={100}
+        // height={50}
         options={{
-          maintainAspectRatio: false,
+          maintainAspectRatio: true,
+          scales: {
+            xAxes: [
+              {
+                display: true,
+                ticks: {
+                  autoSkip: true,
+                  maxTicksLimit: 12,
+                },
+              },
+            ],
+          },
         }}
       />
     </div>
