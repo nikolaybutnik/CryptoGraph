cb71fd49782bd13a098dd828fd23b48bbbfae342
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-04-15 20:08:54 -0400

    feat: re-render the chart when new data is fetched

--- client/src/components/Graph/Graph.js
+++ client/src/components/Graph/Graph.js
@@ -3,14 +3,8 @@ import Chart from 'chart.js'
 import './Graph.css'
 
 const Graph = ({ graphData }) => {
-  const [newGraph, setNewGraph] = useState()
+  const [chart, setChart] = useState(null)
   const graphRef = useRef()
-
-  useEffect(() => {
-    const ctx = graphRef.current.getContext('2d')
-    const graphInstance = new Chart(ctx, {})
-    setNewGraph(graphInstance)
-  }, [])
 
   useEffect(() => {
     const ctx = graphRef.current.getContext('2d')
@@ -37,26 +31,61 @@ const Graph = ({ graphData }) => {
         })
       : null
 
-    // newGraph && console.log(newGraph.config.data)
-    // const newData = newGraph && {
-    //   labels: labels,
-    //   datasets: [
-    //     {
-    //       label: binanceData ? 'Binance' : 'N/A',
-    //       data: binanceData,
-    //     },
-    //     {
-    //       label: kucoinData ? 'KuCoin' : 'N/A',
-    //       data: kucoinData,
-    //     },
-    //   ],
-    // }
-    // newGraph && setNewGraph(...newGraph, newData)
-    // newData && newGraph.update()
-
-    const graphInstance = new Chart(ctx, {
-      type: 'line',
-      data: {
+    if (!chart) {
+      const graphInstance = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: binanceData ? 'Binance' : 'N/A',
+              data: binanceData,
+              fill: false,
+              lineTension: 0.1,
+              backgroundColor: 'rgba(75,192,192,0.4)',
+              borderColor: 'rgba(75,192,192,1)',
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: 'rgba(75,192,192,1)',
+              pointBackgroundColor: '#fff',
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+              pointHoverBorderColor: 'rgba(220,220,220,1)',
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+            },
+            {
+              label: kucoinData ? 'KuCoin' : 'N/A',
+              data: kucoinData,
+              fill: false,
+              lineTension: 0.1,
+              backgroundColor: 'rgba(202,73,73,0.4)',
+              borderColor: 'rgba(202,73,73,1)',
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: 'rgba(202,73,73,1)',
+              pointBackgroundColor: '#fff',
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: 'rgba(202,73,73,1)',
+              pointHoverBorderColor: 'rgba(220,220,220,1)',
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+            },
+          ],
+        },
+      })
+      setChart(graphInstance)
+    } else {
+      console.log(chart.data)
+      chart.data = {
         labels: labels,
         datasets: [
           {
@@ -102,9 +131,10 @@ const Graph = ({ graphData }) => {
             pointHitRadius: 10,
           },
         ],
-      },
-    })
-  }, [graphData])
+      }
+      chart.update()
+    }
+  }, [graphData, chart])
 
   const symbol = graphData.symbol
   const pairSymbol = graphData.pairSymbol
