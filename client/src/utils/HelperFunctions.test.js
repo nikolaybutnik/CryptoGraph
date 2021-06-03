2cdba34b7f786bb9b2c42ad556f3a1e1dd8611d1
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-06-02 21:34:22 -0400

    feat: write additional tests to ensure numberWithCommas function handles big and small numbers correctly

--- client/src/utils/HelperFunctions.test.js
+++ client/src/utils/HelperFunctions.test.js
@@ -1,5 +1,7 @@
 import { numberWithCommas } from './HelperFunctions'
 
 test('Add commas to numbers', () => {
+  expect(numberWithCommas(1000000)).toBe('1,000,000')
   expect(numberWithCommas(1000)).toBe('1,000')
+  expect(numberWithCommas(100)).toBe('100')
 })
