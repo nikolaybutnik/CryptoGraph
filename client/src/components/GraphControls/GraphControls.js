6b4b5119780515f06a3b992c8d437184dc8e603c
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-06-04 20:46:00 -0400

    fix: correct the functionality of trading view, which now always persists

--- client/src/components/GraphControls/GraphControls.js
+++ client/src/components/GraphControls/GraphControls.js
@@ -1,19 +1,8 @@
 import React from 'react'
-import { getGraphData } from '../../utils/ServerCalls'
 
 import './GraphControls.css'
 
-const GraphControls = ({
-  props: {
-    symbol,
-    pairSymbol,
-    setGraphData,
-    viewOption,
-    setViewOption,
-    timeSpan,
-    setTimeSpan,
-  },
-}) => {
+const GraphControls = ({ props: { setViewOption, setTimeSpan } }) => {
   return (
     <div className="graphControls">
       <button
@@ -41,19 +30,12 @@ const GraphControls = ({
         3mn
       </button>
 
-      {/* <div className="viewOptions">
+      <div className="viewOptions">
         <label htmlFor="viewOptions">Trading View: </label>
         <select
           name="viewOptions"
           onChange={(e) => {
             setViewOption(e.target.value)
-            getGraphData(
-              symbol,
-              pairSymbol,
-              setGraphData,
-              timeSpan,
-              e.target.value
-            )
           }}
         >
           <option value="1d">1d</option>
@@ -61,7 +43,7 @@ const GraphControls = ({
           <option value="8h">8h</option>
           <option value="4h">4h</option>
         </select>
-      </div> */}
+      </div>
     </div>
   )
 }
