958102a7255aa8d092d29d30da3e52ce478cc37c
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-06-24 20:17:24 -0400

    refactor: streamline dataset filtering for graph display

--- client/src/utils/HelperFunctions.js
+++ client/src/utils/HelperFunctions.js
@@ -1,4 +1,5 @@
 import { isToday, isYesterday } from 'date-fns'
+import markets from './markets'
 
 // For visual aid only, comma every 3 digits
 const numberWithCommas = (num) => {
@@ -29,72 +30,31 @@ const filterDatasets = (graphData, toggleMarketData) => {
         })
       : null
   } else krakenData = null
+  const dataArray = [binanceData, kucoinData, krakenData]
 
-  let datasets = [
-    {
-      label: 'Binance',
-      data: binanceData,
+  const datasets = markets.map((market, index) => {
+    return {
+      label: market.name,
+      data: dataArray[index],
       fill: false,
       lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
+      backgroundColor: market.chartColor.secondary,
+      borderColor: market.chartColor.primary,
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBorderColor: market.chartColor.primary,
       pointBackgroundColor: '#fff',
       pointBorderWidth: 1,
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBackgroundColor: market.chartColor.primary,
       pointHoverBorderColor: 'rgba(220,220,220,1)',
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-    },
-    {
-      label: 'KuCoin',
-      data: kucoinData,
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(202,73,73,0.4)',
-      borderColor: 'rgba(202,73,73,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(202,73,73,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(202,73,73,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-    },
-    {
-      label: 'Kraken',
-      data: krakenData,
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(255, 210, 70, 0.4)',
-      borderColor: 'rgba(255, 210, 70, 1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(255, 210, 70, 1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(255, 210, 70, 1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-    },
-  ]
+    }
+  })
 
   return datasets.filter((set) => set.data !== null)
 }
--- client/src/utils/markets.js
+++ client/src/utils/markets.js
@@ -1,3 +1,25 @@
-const markets = [{ name: 'Binance' }, { name: 'KuCoin' }, { name: 'Kraken' }]
+const markets = [
+  {
+    name: 'Binance',
+    chartColor: {
+      primary: 'rgba(75,192,192,1)',
+      secondary: 'rgba(75,192,192,0.4)',
+    },
+  },
+  {
+    name: 'KuCoin',
+    chartColor: {
+      primary: 'rgba(202,73,73,1)',
+      secondary: 'rgba(202,73,73,0.4)',
+    },
+  },
+  {
+    name: 'Kraken',
+    chartColor: {
+      primary: 'rgba(255, 210, 70, 1)',
+      secondary: 'rgba(255, 210, 70, 0.4)',
+    },
+  },
+]
 
 export default markets
