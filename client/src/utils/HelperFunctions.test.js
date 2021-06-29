269b2226a9f3d16f0d12e7962f61aa793a231228
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-06-28 21:00:35 -0400

    feat: add tests for isCurrentlyTrading function which returns true if the passed in timestamp is today, and false otherwise

--- client/src/utils/HelperFunctions.test.js
+++ client/src/utils/HelperFunctions.test.js
@@ -1,7 +1,12 @@
-import { numberWithCommas } from './HelperFunctions'
+import { numberWithCommas, isCurrentlyTrading } from './HelperFunctions'
 
 test('Add commas to numbers', () => {
   expect(numberWithCommas(1000000)).toBe('1,000,000')
   expect(numberWithCommas(1000)).toBe('1,000')
   expect(numberWithCommas(100)).toBe('100')
+})
+
+test('Date is today', () => {
+  expect(isCurrentlyTrading(new Date())).toBe(true)
+  expect(isCurrentlyTrading(new Date(2020, 11, 24, 10, 33, 30, 0))).toBe(false)
 })
